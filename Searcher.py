5d94cf6ef963150d69769d406579122177b28581
BEKİR <62219677+bekranker@users.noreply.github.com>
2022-02-03 17:52:51 +0300

    Create Searcher.py

---
+++ Searcher.py
@@ -0,0 +1,40 @@
+import requests
+import os
+import sys
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import os
+import sys
+
+
+
+target = input("Target site: ")
+
+
+def linkSearcher():
+    a = source.find_all("a")
+
+    for i in a:
+        print(i.get("href"))
+        page_0 = open("Links.txt", "a+")
+        page_0.write(i.get("href") + "\n")
+    page_0.close()
+
+
+try:
+    r = requests.get(target)
+    source = BeautifulSoup(r.content, "lxml")
+    targetSiteHeader = source.title
+    print("Target's header: " + targetSiteHeader.text)
+    linkSearcher()
+except:
+    print("Somethings wrong...")
+    pass
+
+
+
+
+
+
+
+
